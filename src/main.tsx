0865c0620053416b72a4c1999fa8ba37ccb58007
Ruslan Karnilov <karnilov.rus@gmail.com>
2025-05-04 19:12:00 +0300

    Подключил алиасы

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "./global/styles/baseStyle.css"
 import { CssBaseline } from '@mui/material'
-import { AppRouter } from './global/router'
+import { AppRouter } from '@global/router'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
