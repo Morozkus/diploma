2e4b203617d30ad3c4cfcd745b82d4adb07e2177
Ruslan Karnilov <karnilov.rus@gmail.com>
2025-05-07 12:22:55 +0300

    ошибки в логине

--- src/modules/LoginForm/helpers/validate.ts
+++ src/modules/LoginForm/helpers/validate.ts
@@ -1,3 +1,3 @@
-export const validateLoginEmail = (email: string) => !/\S+@\S+\.\S+/.test(email)
+export const validateLoginEmail = (email: string) => /\S+@\S+\.\S+/.test(email)
 
-export const validateLoginPassword = (password: string) => password.length < 6
+export const validateLoginPassword = (password: string) => password.length >= 6
