976e05e4fcbc97bfee192c3f82018145b16b61a3
Ruslan Karnilov <karnilov.rus@gmail.com>
2025-06-08 13:15:55 +0300

    modal adaptive

--- src/ui/Modal/components/Modal.tsx
+++ src/ui/Modal/components/Modal.tsx
@@ -1,56 +1,67 @@
 import { Stack, StackProps, SxProps } from "@mui/material";
-import MUIModal from '@mui/material/Modal';
-import { useState, FC, PropsWithChildren, ReactNode } from 'react';
+import MUIModal from "@mui/material/Modal";
+import { useState, FC, PropsWithChildren, ReactNode } from "react";
 
 interface ModalProps extends PropsWithChildren {
-    trigger: ReactNode
+    trigger: ReactNode;
 
-    opened?: boolean
-    onChangeOpened?: (isOpen: boolean) => void
-    closeOnInnerClick?: boolean
+    opened?: boolean;
+    onChangeOpened?: (isOpen: boolean) => void;
+    closeOnInnerClick?: boolean;
 
-    triggerContainerStyles?: StackProps
-    popoverContainerStyles?: StackProps
+    triggerContainerStyles?: StackProps;
+    popoverContainerStyles?: StackProps;
 }
 
-const Modal: FC<ModalProps> = ({ trigger, opened, onChangeOpened, closeOnInnerClick = true, triggerContainerStyles, popoverContainerStyles, children }) => {
+const Modal: FC<ModalProps> = ({
+    trigger,
+    opened,
+    onChangeOpened,
+    closeOnInnerClick = true,
+    triggerContainerStyles,
+    popoverContainerStyles,
+    children,
+}) => {
     const [open, setOpen] = useState(false);
     const handleOpen = () => {
-        setOpen(true)
-        onChangeOpened?.(true)
+        setOpen(true);
+        onChangeOpened?.(true);
     };
     const handleClose = () => {
-        setOpen(false)
-        onChangeOpened?.(false)
+        setOpen(false);
+        onChangeOpened?.(false);
     };
 
-    return <>
-        <Stack onClick={handleOpen} {...triggerContainerStyles}>
-            {trigger}
-        </Stack>
-        <MUIModal
-            open={opened ?? open}
-            onClose={handleClose}
-        >
-            <Stack onClick={closeOnInnerClick ? handleClose : void 0} sx={style} {...popoverContainerStyles}>
-                {children}
+    return (
+        <>
+            <Stack onClick={handleOpen} {...triggerContainerStyles}>
+                {trigger}
             </Stack>
-        </MUIModal>
-    </>
-}
+            <MUIModal open={opened ?? open} onClose={handleClose}>
+                <Stack
+                    onClick={closeOnInnerClick ? handleClose : void 0}
+                    sx={style}
+                    {...popoverContainerStyles}
+                >
+                    {children}
+                </Stack>
+            </MUIModal>
+        </>
+    );
+};
 
 const style: SxProps = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '1px solid #000',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    maxWidth: 400,
+    width: "100%",
+    bgcolor: "background.paper",
+    border: "1px solid #000",
     borderRadius: 2,
     boxShadow: 24,
     padding: 4,
 };
 
-
-export default Modal
+export default Modal;
