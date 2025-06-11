1cb05d877190f1ffd512428a8358b270760202bb
Ruslan Karnilov <karnilov.rus@gmail.com>
2025-06-11 20:35:47 +0300

    убрал сортировку из-за бага при выборе организаций, категорий и статусов

--- src/modules/TechControl/hooks/useTeches.ts
+++ src/modules/TechControl/hooks/useTeches.ts
@@ -7,11 +7,12 @@ import { getOrganizationDocumentByPath } from "@/modules/OrganizationList/store/
 import validateTech from "@/modules/TechControl/api/schema/validateTech";
 import { getTechQueryByFilters } from "@/modules/TechControl/store/collection";
 import { ITech } from "@/global/types/entity/Tech";
-import { orderBy, where } from "firebase/firestore";
+import { where } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { useCurrentStatusId } from "@/global/router/hooks/useSearchParams";
 import { getStatusDocumentByPath } from "@/modules/Status/api/firestore/collection";
 import { useDebouncedSearchTechName } from "@/components/Drawer/hooks/useDebouncedSearchTechName";
+import { useMemo } from "react";
 
 export const useTechesByOrganizationAndCategory = (): [ITech[], boolean] => {
     const organization = useCurrentOrganizationId();
@@ -19,32 +20,36 @@ export const useTechesByOrganizationAndCategory = (): [ITech[], boolean] => {
     const status = useCurrentStatusId();
     const name = useDebouncedSearchTechName();
 
-    const organizationFilter = organization
-        ? where(
-              "organization",
-              "==",
-              getOrganizationDocumentByPath([organization])
-          )
-        : void 0;
-    const categoryFilter = category
-        ? where("category", "==", getCategoryDocumentByPath([category]))
-        : void 0;
-    const statusFilter = status
-        ? where("status", "==", getStatusDocumentByPath([status]))
-        : void 0;
-    const techSubnameStart = name ? where("title", ">=", name) : void 0;
-    const techSubnameEnd = name
-        ? where("title", "<=", name + "\uf8ff")
-        : orderBy("title");
+    const filters = useMemo(() => {
+        const organizationFilter = organization
+            ? where(
+                  "organization",
+                  "==",
+                  getOrganizationDocumentByPath([organization])
+              )
+            : void 0;
+        const categoryFilter = category
+            ? where("category", "==", getCategoryDocumentByPath([category]))
+            : void 0;
+        const statusFilter = status
+            ? where("status", "==", getStatusDocumentByPath([status]))
+            : void 0;
+        const techSubnameStart = name ? where("title", ">=", name) : void 0;
+        const techSubnameEnd = name
+            ? where("title", "<=", name + "\uf8ff")
+            : void 0;
+
+        return [
+            organizationFilter,
+            categoryFilter,
+            statusFilter,
+            techSubnameEnd,
+            techSubnameStart,
+        ];
+    }, [category, name, organization, status]);
 
     const [teches, loading] = useCollectionData(
-        getTechQueryByFilters(
-            categoryFilter,
-            organizationFilter,
-            statusFilter,
-            techSubnameStart,
-            techSubnameEnd
-        ),
+        getTechQueryByFilters(...filters),
         { initialValue: [] }
     );
 
